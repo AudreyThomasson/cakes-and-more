3f8be4ae15c47a8496149de89cbcc4ca9f80d370
Audrey Thomasson <audrey@macbook-pro.lan>
2020-08-28 15:26:36 -0500

    displays cakes

--- scripts/Bakery.js
+++ scripts/Bakery.js
@@ -1,6 +1,16 @@
 let bakedCakes = [];
 
-export const doTheBaking = (tacoCake) => {
-    tacoCake.baked = true;
+// adds .baked to each cake item
+export const doTheBaking = (tacoCake, cold) => {
     
+    if (cold === true) {
+        tacoCake.baked = false;
+    } else {
+        tacoCake.baked = true;
+    }
+    bakedCakes.push(tacoCake);
+}
+
+export const readyCakes = () => {
+    return bakedCakes.slice();
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,20 @@
 console.log("Welcome to the main module");
 import { MakeCake } from "./MakeCake.js";
 import { doTheBaking } from "./Bakery.js";
+import { CakeList } from "./CakeList.js";
 
 // let's pretend we just filled out some input boxes with the
 // following information
 // assigning it to cake1 so we can use it later
 const cake1 = MakeCake("Christmas In July", "Summer", "white", "cake", "vanilla");
-console.log(cake1);
+// console.log(cake1);
 
 const cake2 = MakeCake("Super Bowl Brownies", "Winter", "brown", "brownie", "fudge");
-console.log(cake2);
+// console.log(cake2);
 
 
-doTheBaking(cake2);
+doTheBaking(cake2, false);
+doTheBaking(cake1, true);
+
+const contentTarget = document.querySelector("#cakeDisplay");
+contentTarget.innerHTML = CakeList();
